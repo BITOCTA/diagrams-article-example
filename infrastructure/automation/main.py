2ee5b50c08448f7f187a840a4f9e15411d69b131
BITOCTA <artso003@gmail.com>
2021-06-08 16:59:11 +0200

    add connection outside of VPC, automation and folder structure

--- infrastructure/automation/main.py
+++ infrastructure/automation/main.py
@@ -6,19 +6,75 @@ stack_name = "dev"
 
 work_dir = os.path.join(os.path.dirname(__file__), "..", "src")
 
+print("Preparing pulumi environment")
 
-subprocess.run(["python3", "-m", "venv", "venv"], check=True, cwd=work_dir, capture_output=True)
-subprocess.run([os.path.join("venv", "bin", "python3"), "-m", "pip", "install", "--upgrade", "pip"],
-               check=True, cwd=work_dir, capture_output=True)
-subprocess.run([os.path.join("venv", "bin", "pip"), "install", "-r", "requirements.txt"],
-               check=True, cwd=work_dir, capture_output=True)
+subprocess.run(
+    ["python3", "-m", "venv", "venv"], check=True, cwd=work_dir, capture_output=True
+)
+subprocess.run(
+    [
+        os.path.join("venv", "bin", "python3"),
+        "-m",
+        "pip",
+        "install",
+        "--upgrade",
+        "pip",
+    ],
+    check=True,
+    cwd=work_dir,
+    capture_output=True,
+)
+subprocess.run(
+    [os.path.join("venv", "bin", "pip"), "install", "-r", "requirements.txt"],
+    check=True,
+    cwd=work_dir,
+    capture_output=True,
+)
 
 stack = auto.create_or_select_stack(stack_name="dev", work_dir=work_dir)
 
 stack.set_config("aws:region", auto.ConfigValue(value="us-east-1"))
 
-stack_preview = stack.up()
+stack_preview = stack.up(on_output=print)
+
+subprocess.run(
+    ["pulumi", "stack", "export", "--file", "stack.json"],
+    check=True,
+    cwd=work_dir,
+    capture_output=True,
+)
+
+work_dir = os.path.join(os.path.dirname(__file__), "..", "diagrams")
 
 
+print("Preparing diagrams environment")
 
-print(stack_preview.outputs)
+subprocess.run(
+    ["python3", "-m", "venv", "venv"], check=True, cwd=work_dir, capture_output=True
+)
+subprocess.run(
+    [
+        os.path.join("venv", "bin", "python3"),
+        "-m",
+        "pip",
+        "install",
+        "--upgrade",
+        "pip",
+    ],
+    check=True,
+    cwd=work_dir,
+    capture_output=True,
+)
+subprocess.run(
+    [os.path.join("venv", "bin", "pip"), "install", "-r", "requirements.txt"],
+    check=True,
+    cwd=work_dir,
+    capture_output=True,
+)
+
+subprocess.run(
+    [os.path.join("venv", "bin", "python3"), "diagrams_generator.py"],
+    check=True,
+    cwd=work_dir,
+    capture_output=True,
+)
